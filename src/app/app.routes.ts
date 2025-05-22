1da20afc2715933b0737bc332c544082f155d680
Thomas Andersen <55490372+Thekid2002@users.noreply.github.com>
2025-05-22 16:53:40 +0200

    hello

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {BreakTimerComponent} from './break-timer/break-timer.component';
 import {BreakWorkHistoryComponent} from './break-work-history/break-work-history.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'history', pathMatch: 'full' },
   { path: 'home', component: BreakTimerComponent},
   { path: 'history', component: BreakWorkHistoryComponent}
 ];
